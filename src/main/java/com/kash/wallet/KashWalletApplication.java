ca5c8890463c68e15c4be8ef51a478be00bdd779
Joel George <joelngeorge@outlook.com>
2021-05-30 00:55:58 +0530

    Initial Commit

---
+++ src/main/java/com/kash/wallet/KashWalletApplication.java
@@ -0,0 +1,13 @@
+package com.kash.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KashWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KashWalletApplication.class, args);
+	}
+
+}
